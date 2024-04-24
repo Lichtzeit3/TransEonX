6c97e64061f29d780c03ab64b3a9e6d1be735f29
Lichtzeit <sdchang3@gmail.com>
2024-04-24 16:10:33 +0800

    hamburger fixed

--- JS/hamburger.js
+++ JS/hamburger.js
@@ -1,5 +1,18 @@
 
-function toggleClass() {
-    let element = document.getElementById("hamburger");
-    element.classList.toggle("active");
+window.addEventListener('load', doFirst);
+
+function doFirst() {
+    let element = document.querySelectorAll('.navbar');
+    let ham = document.getElementById("hamburger");
+
+    function toggleClass() {
+        ham.classList.toggle("active");
+    }
+
+    ham.addEventListener('click', () => {
+        element.forEach(ul => {
+            ul.style.display = ul.style.display === 'flex' ? 'none' : 'flex';
+        });
+        toggleClass();
+    });
 }
