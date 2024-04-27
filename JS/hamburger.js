10b3089000f215f2eeabdf7f048fcd27f9d817d2
Lichtzeit <sdchang3@gmail.com>
2024-04-27 15:42:29 +0800

    RWD index, nier and ffvii done

--- JS/hamburger.js
+++ JS/hamburger.js
@@ -15,4 +15,16 @@ function doFirst() {
         });
         toggleClass();
     });
+
+    let element1 = document.querySelector('.nav');
+    let ham1 = document.getElementById("hamburger");
+
+    function toggleClass() {
+        ham1.classList.toggle("active");
+    }
+
+    ham1.addEventListener('click', () => {
+        element1.style.display = element1.style.display === 'none' ? 'block' : 'none';
+        toggleClass();
+    });
 }
